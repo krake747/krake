17fb8a21fd45efed07207730d6692b0d58fbeace
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-02-06 23:14:14 +0100

    feat(python): some portfolio theory

--- python/finance/portfolio.py
+++ python/finance/portfolio.py
@@ -1,6 +1,39 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
+
+
+def expected_mu(weights: list[float], means: list[float]) -> float:
+    w = np.array(weights)
+    mu = np.array(means)
+    mu_p = mu @ w.T
+    return float(mu_p)
+
+
+def expected_std(weights: list[float], cov_matrix: list[list[float]]) -> float:
+    w = np.array(weights)
+    cov = np.array(cov_matrix)
+    sigma_p = (w @ cov @ w) ** 0.5
+    return float(sigma_p)
+
+
+def covariance_matrix(sigma: list[float], corr_matrix: list[list[float]]) -> list[list[float]]:
+    cov = np.diag(sigma) @ np.array(corr_matrix) @ np.diag(sigma)
+    return cov.tolist()
+
+
+def correlation_matrix(sigma: list[float], cov_matrix: list[list[float]]) -> list[list[float]]:
+    raise NotImplementedError()
+
+
+def mu_sigma_portfolio(weights: list[float], means: list[float], cov_matrix: list[list[float]]) -> tuple[float, float]:
+    w = np.array(weights)
+    mu = np.array(means)
+    cov = np.array(cov_matrix)
+    mu_p = mu @ w.T
+    sigma_p = (w @ cov @ w.T) ** 0.5
+    return float(mu_p), float(sigma_p)
 
 
 def plot_mu_sigma(mu: list[float], sigma: list[float], stocks: list[str]) -> tuple[Figure, Axes]:
--- python/main.py
+++ python/main.py
@@ -1,14 +1,33 @@
 from sys import exit
 import finance.portfolio as pf
+import numpy as np
 
 
 def main():
     stocks = ['A', 'B', 'C']
-    mu = [0.06, 0.10, 0.14]
-    sigma = [0.16, 0.22, 0.38]
-    fig, _ = pf.plot_mu_sigma(mu, sigma, stocks)
-    fig.show()
-    input("Close the figure and press a key to continue")
+    means = [0.04, 0.09, 0.12]
+    sigma = [0.15, 0.20, 0.35]
+    weights = [0.2, 0.3, 0.5]
+
+    mu_p = pf.expected_mu(weights, means)
+    print('Expected portfolio return: ', mu_p)
+
+    corr = [[1.0, 0.1, 0.17],
+            [0.1, 1.0, 0.26],
+            [0.17, 0.26, 1.0]]
+
+    cov = pf.covariance_matrix(sigma, corr)
+    print('Covariance matrix: \n', cov)
+
+    sigma_p = pf.expected_std(weights, cov)
+    print('Portfolio standard deviation: ', sigma_p)
+
+    mu_mvp, sigma_mvp = pf.mu_sigma_portfolio(weights, means, cov)
+    print("Mvp portfolio mu, sigma: ", mu_mvp, sigma_mvp)
+    
+    # fig, _ = pf.plot_mu_sigma(mu, sigma, stocks)
+    # fig.show()
+    # input("Close the figure and press a key to continue")
 
 
 if __name__ == "__main__":
