5933aef5064d8b03677c85042adff59683adda20
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-02-08 21:40:39 +0100

    feat(fsharp): mu sigma portfolio

---
+++ samples/Krake.App.Finance/Program.fs
@@ -0,0 +1,14 @@
+﻿open Krake.FSharp
+let stocks = seq [ "A"; "B"; "C" ]
+let means = seq [ 0.04; 0.09; 0.12 ]
+let sigma = seq [ 0.15; 0.20; 0.35 ]
+let weights = seq [ 0.2; 0.3; 0.5 ]
+let rf = 0.02
+
+let corrMatrix =
+    seq [ seq [ 1.0; 0.1; 0.17 ]; [ 0.1; 1.0; 0.26 ]; [ 0.17; 0.26; 1.0 ] ]
+
+let covMatrix = Portfolio.covarianceMatrix sigma corrMatrix
+
+let muPf, sigmaPf = Portfolio.muSigmaPortfolio weights means covMatrix
+printfn $"Expected portfolio return {muPf} and standard deviation: {sigmaPf}"
--- src/Krake.FSharp/Library.fs
+++ src/Krake.FSharp/Library.fs
@@ -1,5 +1,26 @@
 ﻿namespace Krake.FSharp
 
-module Say =
-    let hello name =
-        printfn $"Hello %s{name}"
+open FSharp.Stats
+
+
+module Portfolio =
+    let expectedMu weights means =
+        let w = vector weights
+        let mu = vector means
+        let muPf = mu.Transpose * w
+        muPf
+
+    let expectedStd weights (covMatrix: float seq seq) =
+        let w = vector weights
+        let cov = matrix covMatrix
+        let sigmaPf = (w.Transpose * cov * w) ** 0.5
+        sigmaPf
+
+    let covarianceMatrix sigma (corrMatrix: float seq seq) =
+        let cov = (Matrix.diag (vector sigma)) * (matrix corrMatrix) * (Matrix.diag (vector sigma))
+        Matrix.toJaggedSeq cov
+
+    let muSigmaPortfolio weights means (covMatrix: float seq seq) =
+        let muPf = expectedMu weights means
+        let sigmaPf = expectedStd weights covMatrix
+        muPf, sigmaPf
