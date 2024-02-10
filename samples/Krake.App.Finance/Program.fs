5352d321d6b833bd7790cd5f96ad9a54f2ab8684
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-02-10 17:51:09 +0100

    feat(sharp): Create combined mu sigma portfolio chart

--- samples/Krake.App.Finance/Program.fs
+++ samples/Krake.App.Finance/Program.fs
@@ -1,12 +1,13 @@
 ﻿open Krake.FSharp
-let stocks = seq [ "A"; "B"; "C" ]
-let means = seq [ 0.04; 0.09; 0.12 ]
-let sigma = seq [ 0.15; 0.20; 0.35 ]
-let weights = seq [ 0.2; 0.3; 0.5 ]
+open Plotly.NET
+
+let stocks = [ "A"; "B"; "C" ]
+let means = [ 0.04; 0.09; 0.12 ]
+let sigma = [ 0.15; 0.20; 0.35 ]
+let weights = [ 0.2; 0.3; 0.5 ]
 let rf = 0.02
 
-let corrMatrix =
-    seq [ seq [ 1.0; 0.1; 0.17 ]; [ 0.1; 1.0; 0.26 ]; [ 0.17; 0.26; 1.0 ] ]
+let corrMatrix = [ [ 1.0; 0.1; 0.17 ]; [ 0.1; 1.0; 0.26 ]; [ 0.17; 0.26; 1.0 ] ]
 
 let covMatrix = Portfolio.covarianceMatrix sigma corrMatrix
 
@@ -14,4 +15,9 @@ let muPf, sigmaPf = Portfolio.muSigma weights means covMatrix
 
 printfn $"Expected portfolio return {muPf} and standard deviation: {sigmaPf}"
 
-PortfolioCharts.plotMuSigma means sigma stocks
+[
+    PortfolioCharts.chartRandomPortfolios means covMatrix 1000 |> Chart.withTraceInfo "random"
+    PortfolioCharts.chartMuSigma means sigma stocks |> Chart.withTraceInfo "mu-sigma"
+]
+|> Chart.combine
+|> Chart.show
--- src/Krake.FSharp/Library.fs
+++ src/Krake.FSharp/Library.fs
@@ -1,37 +1,70 @@
 ﻿namespace Krake.FSharp
 
 open FSharp.Stats
+open Plotly.NET.StyleParam
 
 
 module Portfolio =
-    let expectedMu weights means =
+    let expectedMu (weights: float seq) (means: float seq) =
         let w = vector weights
         let mu = vector means
         let muPf = mu.Transpose * w
+
         muPf
 
-    let expectedStd weights (covMatrix: float seq seq) =
+    let expectedStd (weights: float list) (covMatrix: float list list) =
         let w = vector weights
         let cov = matrix covMatrix
         let sigmaPf = (w.Transpose * cov * w) ** 0.5
+
         sigmaPf
 
-    let covarianceMatrix sigma (corrMatrix: float seq seq) =
-        let cov = (Matrix.diag (vector sigma)) * (matrix corrMatrix) * (Matrix.diag (vector sigma))
-        Matrix.toJaggedSeq cov
+    let covarianceMatrix (sigma: float list) (corrMatrix: float list list) =
+        let cov =
+            (Matrix.diag (vector sigma))
+            * (matrix corrMatrix)
+            * (Matrix.diag (vector sigma))
 
-    let muSigma weights means (covMatrix: float seq seq) =
+        Matrix.toJaggedSeq cov |> JaggedList.ofJaggedSeq
+
+    let muSigma weights (means: float list) (covMatrix: float list list) =
         let muPf = expectedMu weights means
         let sigmaPf = expectedStd weights covMatrix
+
         muPf, sigmaPf
 
 module PortfolioCharts =
     open Plotly.NET
+    open Plotly.NET.TraceObjects
+    open FSharp.Stats.Distributions
 
-    let plotMuSigma (means: float seq) (sigma: float seq) (stocks: string seq) =
-        let x = sigma
-        let y = means
-        Chart.Scatter(x, y, MultiText = stocks, TextPosition = StyleParam.TextPosition.TopRight, mode=StyleParam.Mode.Markers_Text)
-        |> Chart.withXAxisStyle(TitleText = "Standard Deviation", MinMax = (0, 0.45))
-        |> Chart.withYAxisStyle(TitleText = "Mean", MinMax = (0, 0.25))
-        |> Chart.show
+    let chartMuSigma (means: float list) (sigma: float list) (stocks: string list) =
+        Chart.Scatter(
+            x = sigma,
+            y = means,
+            MultiText = stocks,
+            TextPosition = TextPosition.TopRight,
+            Marker = Marker.init (Color = Color.fromKeyword Black),
+            mode = Mode.Markers_Text
+        )
+        |> Chart.withTitle "Mu-Sigma Chart"
+        |> Chart.withXAxisStyle (TitleText = "Standard Deviation", MinMax = (0, 0.45))
+        |> Chart.withYAxisStyle (TitleText = "Mean", MinMax = (0, 0.25))
+
+
+    let chartRandomPortfolios (means: float list) (covMatrix: float list list) (nSimulations: int) =
+        let randomWeights (nAssets: int) : float list =
+            let normal = Continuous.Normal.Init 0 1
+            let k = Vector.init nAssets (fun _ -> normal.Sample())
+            let sum = Vector.sum k
+            k |> Vector.map (fun x -> x / sum) |> Vector.toArray |> Array.toList
+
+        let nAssets = Seq.length means
+
+        let simulations =
+            [ 0..nSimulations ]
+            |> List.map (fun _ -> Portfolio.muSigma (randomWeights nAssets) means covMatrix)
+
+        Chart.Scatter(x = (List.map snd simulations), y = (List.map fst simulations), mode = Mode.Markers_Text)
+        |> Chart.withXAxisStyle (TitleText = "Standard Deviation", MinMax = (0, 0.45))
+        |> Chart.withYAxisStyle (TitleText = "Mean", MinMax = (0, 0.25))
