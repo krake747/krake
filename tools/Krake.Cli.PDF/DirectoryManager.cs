44d0357ece177d3df3b17cfbd985a095a5e4c3a2
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-03-07 21:07:28 +0100

    feat(tools): init pdf generator tool

---
+++ tools/Krake.Cli.PDF/DirectoryManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Krake.Cli.PDF;
+
+public sealed class DirectoryManager(string rootDirectory)
+{
+    private const string InFolder = "In";
+    private const string OutFolder = "Out";
+
+    public DirectoryInfo Root { get; } = CreateDirectory(rootDirectory);
+    public DirectoryInfo In { get; } = CreateDirectory(Path.Combine(rootDirectory, InFolder));
+    public DirectoryInfo Out { get; } = CreateDirectory(Path.Combine(rootDirectory, OutFolder));
+
+    private static DirectoryInfo CreateDirectory(string path) =>
+        Directory.Exists(path) is false ? Directory.CreateDirectory(path) : new DirectoryInfo(path);
+}
