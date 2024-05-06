f01194e3f385fecd0adaf25ded6737551368050c
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-05-06 18:37:15 +0200

    refactor(core): Refactor database type handlers

---
+++ src/Krake.Core.Infrastructure/Data/TypeHandlers/TimeOnlyTypeHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Dapper;
+
+namespace Krake.Core.Infrastructure.Data.TypeHandlers;
+
+internal sealed class TimeOnlyTypeHandler : SqlMapper.TypeHandler<TimeOnly>
+{
+    public override TimeOnly Parse(object value) => value switch
+    {
+        DateTime dateTime => TimeOnly.FromDateTime(dateTime),
+        TimeSpan timeSpan => TimeOnly.FromTimeSpan(timeSpan),
+        _ => default
+    };
+
+    public override void SetValue(IDbDataParameter parameter, TimeOnly value)
+    {
+        parameter.DbType = DbType.Time;
+        parameter.Value = value;
+    }
+}
