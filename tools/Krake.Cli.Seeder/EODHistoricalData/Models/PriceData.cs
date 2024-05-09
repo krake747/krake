752c5df738a711a524f2096d701276b711e9a7da
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-05-09 11:27:36 +0200

    fix(tools): Fix csv PriceDataMap [skip ci]

--- tools/Krake.Cli.Seeder/EODHistoricalData/Models/PriceData.cs
+++ tools/Krake.Cli.Seeder/EODHistoricalData/Models/PriceData.cs
@@ -6,17 +6,11 @@ namespace Krake.Cli.EODHistoricalData.EODHistoricalData.Models;
 public sealed record PriceData
 {
     [JsonPropertyName("date")] public required string Date { get; init; }
-
     [JsonPropertyName("open")] public required decimal Open { get; init; }
-
     [JsonPropertyName("high")] public required decimal High { get; init; }
-
     [JsonPropertyName("low")] public required decimal Low { get; init; }
-
     [JsonPropertyName("close")] public required decimal Close { get; init; }
-
     [JsonPropertyName("adjusted_close")] public required decimal AdjustedClose { get; init; }
-
     [JsonPropertyName("volume")] public required decimal Volume { get; init; }
 }
 
@@ -30,5 +24,6 @@ internal sealed class PriceDataMap : ClassMap<PriceData>
         Map(p => p.Low).Index(3).Name(nameof(PriceData.Low));
         Map(p => p.Close).Index(4).Name(nameof(PriceData.Close));
         Map(p => p.AdjustedClose).Index(5).Name(nameof(PriceData.AdjustedClose));
+        Map(p => p.Volume).Index(6).Name(nameof(PriceData.Volume));
     }
 }
